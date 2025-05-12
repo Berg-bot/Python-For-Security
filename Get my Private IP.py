aca15dc1959eae288ed020c01e673062daa32d2f
Daniel Berg <danielbviegas1@gmail.com>
2025-05-12 14:08:42 -0300

    Update Get my Private IP.py

--- Get my Private IP.py
+++ Get my Private IP.py
@@ -1,1 +1,12 @@
+import socket
 
+def get_private_ip():
+    try:
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+        print(f"Your private IP address is: {local_ip}")
+    except socket.error as e:
+        print(f"An error occurred while getting the private IP address: {e}")
+
+if __name__ == "__main__":
+    get_private_ip()
